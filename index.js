658e5c07fb684982ce1a55e4c6f6006cfe48b60a
Lizzie <lchan217@gmail.com>
2020-01-22 14:47:40 -0700

    add error message & visibility

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
-let form = document.querySelector("form");
-let emailInput = document.querySelector(".email-input");
-let errorMessage = document.querySelector(".error-message");
+document.addEventListener("DOMContentLoaded", function(event) {
+  let form = document.querySelector("form");
+  let emailInput = document.querySelector(".email-input");
+  let errorMessage = document.querySelector(".error-message");
 
-let validEmail = email => {
-  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-let handleSubmit = event => {
-  event.preventDefault();
-  if (validEmail(emailInput.value)) {
-    // Add hidden class to error message
-    //Remove border-danger from emailInput if it exists
-    if (emailInput.classList.contains("error-border")) {
-      emailInput.classList.remove("error-border");
+  let validEmail = email => {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+      return true;
+    } else {
+      return false;
     }
-  } else {
-    // Remove hidden class from error message if it exists
-    // Add border-danger to emailInput
-    if (!emailInput.classList.contains("error-border")) {
-      emailInput.classList.add("error-border");
-    }
-  }
-};
+  };
 
-form.onsubmit = handleSubmit;
+  errorMessage.style.visibility = "hidden";
+
+  let handleSubmit = event => {
+    event.preventDefault();
+    if (validEmail(emailInput.value)) {
+      // Add hidden class to error message
+      errorMessage.style.visibility = "hidden";
+
+      //Remove border-danger from emailInput if it exists
+      if (emailInput.classList.contains("error-border")) {
+        emailInput.classList.remove("error-border");
+      }
+    } else {
+      // Remove hidden class from error message if it exists
+      errorMessage.style.visibility = "visible";
+
+      // Add border-danger to emailInput
+      if (!emailInput.classList.contains("error-border")) {
+        emailInput.classList.add("error-border");
+      }
+    }
+  };
+
+  form.onsubmit = handleSubmit;
+});
